6a8bb55910ae02319e5c234fe311f8af193177a8
25312 <2531235521@qq.com>
2022-07-18 20:54:47 +0800

    修正错误

---
+++ vue/vue01/src/views/induction/StatusSearch.vue
@@ -0,0 +1,47 @@
+<template>
+	<div id="container">
+		<el-form-item>
+		<el-button type="primary" size='large' @click="search1()">搜索</el-button>
+	</el-form-item>
+	<el-form-item>
+		<el-button type="primary" size='large' @click="search2()">搜索</el-button>
+	</el-form-item>
+	</div>
+</template>
+
+<script>
+	export default{
+		name:"StatusSearch",
+		data(){
+			return{
+				staff1:{
+					
+				},
+				users:[]
+			}
+		},
+		methods:{
+			search1(){
+				this.$axios.post("http://localhost:8088/demo/user/search",this.user)
+				.then(rst=>{
+					//console.log(rst.data)
+					this.users=rst.data.rst;
+				}).catch(err=>{
+					console.log(err)
+				})
+			},
+			search2(){
+				this.$axios.post("http://localhost:8088/demo/user/search",this.user)
+				.then(rst=>{
+					//console.log(rst.data)
+					this.users=rst.data.rst;
+				}).catch(err=>{
+					console.log(err)
+				})
+			}
+		}
+	}
+</script>
+
+<style>
+</style>
