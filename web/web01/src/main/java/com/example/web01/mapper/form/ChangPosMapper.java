f2e75994c38be3baa9466ffe7cbc64b5d7708344
Gao Jirui <18245371281@163.com>
2022-07-18 20:51:31 +0800

    岗位调换报表

---
+++ web/web01/src/main/java/com/example/web01/mapper/form/ChangPosMapper.java
@@ -0,0 +1,14 @@
+package com.example.web01.mapper.form;
+
+import com.example.web01.dto.form.ChangPosDto;
+import com.example.web01.po.form.ChangPos;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface ChangPosMapper {
+
+    List<ChangPos> findByParam(ChangPosDto dto);
+
+}
